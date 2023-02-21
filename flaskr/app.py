159e0c32748daf754cd71b1257b0a2171e0b3255
Karthik Vignesh Sundar <karthik.vignesh.sun@gmail.com>
2023-02-21 22:55:31 +0000

    added table structure

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,3 +1,42 @@
 import flask
-import sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy.orm import Session, registry
 
+mapper_registry = registry()
+Base = mapper_registry.generate_base()
+
+class JobInfoTable(Base):
+    __tablename__ = 'JobTrackerTable'
+    id = Column(Integer, primary_key=True)
+    job = Column(String(50))
+    company = Column(String(100))
+    salary = Column(Integer)
+    location = Column(String(100))
+    jobStartDate = Column(Date)
+    jobApplicationClosingDate = Column(Date)
+    status = Column(String(20))
+    notes = Column(String(2000))
+    startJobTrackDate = Column(Date)
+    modifiedJobTrackDate = Column(Date)
+    def __repr__(self):
+        return f"Table recording all job tracking information"
+
+class DatabaseHandler:
+    def __init__(self):
+        self.engine = create_engine("sqlite+pysqlite:///jobDatabase.db", echo=True, future=True)
+
+    def addRow(self, JobInfo: dict):
+        pass
+
+    def deleteRow(self, id: int):
+        pass
+
+    def updateRow(self, id: int, columnValues: dict):
+        pass
+
+    def retreiveRows(self, id: int, searchFilters: dict) -> dict:
+        pass
+
+
+if __name__ == "__main__":
+    pass
