8d7ec54f24b3b298a8829b0030d015672701e8ee
Karthik Vignesh Sundar <karthik.vignesh.sun@gmail.com>
2023-02-22 00:10:02 +0000

    changed the location and creation of the JobTrackerTable class

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,32 +1,48 @@
 import flask
-from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy import create_engine, Column, Integer, String, Date, Table
 from sqlalchemy.orm import Session, registry
 
-mapper_registry = registry()
-Base = mapper_registry.generate_base()
 
-class JobInfoTable(Base):
-    __tablename__ = 'JobTrackerTable'
-    id = Column(Integer, primary_key=True)
-    job = Column(String(50))
-    company = Column(String(100))
-    salary = Column(Integer)
-    location = Column(String(100))
-    jobStartDate = Column(Date)
-    jobApplicationClosingDate = Column(Date)
-    status = Column(String(20))
-    notes = Column(String(2000))
-    startJobTrackDate = Column(Date)
-    modifiedJobTrackDate = Column(Date)
-    def __repr__(self):
-        return f"Table recording all job tracking information"
 
 class DatabaseHandler:
     def __init__(self):
         self.engine = create_engine("sqlite+pysqlite:///jobDatabase.db", echo=True, future=True)
+        self.mapper_registry = registry()
+        self.Base = self.mapper_registry.generate_base()
+        if self.engine.dialect.has_table(self.engine, "JobTrackerTable"):
+            self.JobTrackerTable = self.getTable()
+        else:
+            self.JobTrackerTable = self.createTable()
+
+    def createTable(self):
+        class JobTrackerTable(self.Base):
+            __tablename__ = "JobTrackerTable"
+            id = Column(Integer, primary_key=True)
+            job = Column(String(50))
+            company = Column(String(100))
+            salary = Column(Integer)
+            location = Column(String(100))
+            jobStartDate = Column(Date)
+            jobApplicationClosingDate = Column(Date)
+            status = Column(String(20))
+            notes = Column(String(2000))
+            startJobTrackDate = Column(Date)
+            modifiedJobTrackDate = Column(Date)
+            def __repr__(self):
+                return f"Table recording all job tracking information"
+        self.mapper_registry.metadata.create_all()
+        return JobTrackerTable
+
+    def getTable(self):
+        class JobTrackerTable(self.Base):
+            __table__ = Table("JobTrackerTable", 
+                              self.mapper_registry.metadata, 
+                              autoload_with=self.engine)
+        return JobTrackerTable
 
     def addRow(self, JobInfo: dict):
-        pass
+        with Session(self.engine) as session:
+            pass
 
     def deleteRow(self, id: int):
         pass
