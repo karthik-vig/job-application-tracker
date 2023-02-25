7386a2b4672e64b983637d98b47b8717896b7184
Karthik Vignesh Sundar <karthik.vignesh.sun@gmail.com>
2023-02-25 01:58:02 +0000

    corrected mistakes in index.html and did manual testing in app.py

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from database import DatabaseHandler                                      
                             
+app = Flask(__name__)
 
-
-if __name__ == "__main__":
-    pass
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    jobInofList = [ {'job': 'auditor', 'company': 'kpmg', 'salary': 50000}, 
+    {'job': 'auditor2', 'company': 'pwc', 'salary': 45000}]
+    #'''
+    if request.method == 'POST':
+        searchFilters = {'searchText': request.form['searchText'],
+                        'salary': { 'min': request.form['salaryMin'],
+                                    'max': request.form['salaryMax']
+                                    },
+                        'jobStartDate': request.form['jobStartDate'],
+                        'applicationStatus': request.form['applicationStatus'],
+                        'jobLocation': request.form['jobLocation']
+                        }
+    #'''
+    print(searchFilters)
+    return render_template('index.html', jobInfoList=jobInofList,
+                             jobLocations=['glasgow', 'edingburgh'])
