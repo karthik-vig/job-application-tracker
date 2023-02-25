a4deec9cccdf01bc44c55434c11ff131d6af62bb
Karthik Vignesh Sundar <karthik.vignesh.sun@gmail.com>
2023-02-25 21:33:31 +0000

    fixed modifyJobInfo html file and added the necessary changes in other files

--- flaskr/app.py
+++ flaskr/app.py
@@ -6,33 +6,73 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
-    jobInofList = [ {'job': 'auditor', 'company': 'kpmg', 'salary': 50000}, 
-    {'job': 'auditor2', 'company': 'pwc', 'salary': 45000}]
+    jobInofList = [ {   'id': 1,
+                        'job': 'auditor', 
+                        'company': 'kpmg', 
+                        'salary': 50000,
+                        'jobLocation': 'glasgow',
+                        'jobStartDate': '2023-05-01',
+                        'jobApplicationClosingDate': '2023-04-01',
+                         'applicationStatus': 'Applied',
+                         'notes': 'nice job'
+                        }, 
+                        {'id': 2,
+                        'job': 'auditor2',
+                         'company': 'pwc',
+                          'salary': 45000,
+                          'jobLocation': 'london',
+                        'jobStartDate': '2023-05-01',
+                        'jobApplicationClosingDate': '2023-04-01',
+                         'applicationStatus': 'Applied',
+                         'notes': 'nicer job'
+                        } 
+                ]
     return render_template('index.html', jobInfoList=jobInofList,
                              jobLocations=['glasgow', 'edingburgh'])
 
 
-@app.route('/addJobInfo', methods=['GET', 'POST'])
+@app.route('/addJobInfo', methods=['GET'])
 def addJobInfo():
-    if request.method == 'POST':
-        jobInfo = {'job': request.form['job'],
-                    'company': request.form['company'],
-                    'salary': request.form['salary'],
-                    'jobLocation': request.form['jobLocation'],
-                    'jobStartDate': request.form['jobStartDate'],
-                    'jobApplicationClosingDate': request.form['jobApplicationClosingDate'],
-                    'applicationStatus': request.form['applicationStatus'],
-                    'notes': request.form['notes']
-                    }
     return render_template('addJobInfo.html')
 
 
+@app.route('/modifyJobInfo/', methods=['GET'])
+def modifyJobInfo():
+    id = request.args.get('id')
+    print(id)
+    jobInfo = { 'job': 'auditor', 
+                'company': 'kpmg', 
+                'salary': 50000,
+                'jobLocation': 'glasgow',
+                'jobStartDate': '2023-05-01',
+                'jobApplicationClosingDate': '2023-04-01',
+                'statusApplied': '',
+                'statusIReject': '',
+                'statusTheyReject': 'selected',
+                'statusSuccess': '',
+                'notes': 'nice job',
+                'startJobTrackDate': '2023-02-24',
+                'modifiedJobTrackDate': '2023-02-25'
+                }
+    return render_template('modifyJobInfo.html', jobInfo=jobInfo)
+
+
 @app.route('/redirectIndex', methods=['POST'])
+@app.route('/redirectAddJobInfo', methods=['POST'])
+@app.route('/redirectModifyJobInfo', methods=['POST'])
+@app.route('/redirect', methods=['POST'])
 def redirect():
     requestUrl = str(request.url_rule)
+    urlToGet = '/index'
     if requestUrl == '/redirectIndex':
-        urlToGet = requestUrl
         searchFilters = getSearchFilters()
+        print(searchFilters)
+    elif requestUrl == '/redirectAddJobInfo':
+        jobInfo = getAddJobInfo()
+        print(jobInfo)
+    elif requestUrl == '/redirectModifyJobInfo':
+        modifiedJobInfo = getModifiedJobInfo()
+        print(modifiedJobInfo)
     return render_template('redirect.html', urlToGet=urlToGet)
 
 
@@ -45,5 +85,30 @@ def getSearchFilters():
                     'applicationStatus': request.form['applicationStatus'],
                     'jobLocation': request.form['jobLocation']
                     }
-    print(searchFilters)
     return searchFilters
+
+
+def getAddJobInfo():
+    jobInfo = {'job': request.form['job'],
+        'company': request.form['company'],
+        'salary': request.form['salary'],
+        'jobLocation': request.form['jobLocation'],
+        'jobStartDate': request.form['jobStartDate'],
+        'jobApplicationClosingDate': request.form['jobApplicationClosingDate'],
+        'applicationStatus': request.form['applicationStatus'],
+        'notes': request.form['notes']
+        }
+    return jobInfo
+
+
+def getModifiedJobInfo():
+    modifiedJobInfo = { 'job': request.form['job'], 
+                        'company': request.form['company'], 
+                        'salary': request.form['salary'],
+                        'jobLocation': request.form['jobLocation'],
+                        'jobStartDate': request.form['jobStartDate'],
+                        'jobApplicationClosingDate': request.form['jobApplicationClosingDate'],
+                        'applicationStatus': request.form['applicationStatus'],
+                        'notes': request.form['job'],
+                    }
+    return modifiedJobInfo
