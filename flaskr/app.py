34fb6e4de6cd7094e51195f623d5a610cf108707
Karthik Vignesh Sundar <karthik.vignesh.sun@gmail.com>
2023-02-25 19:13:33 +0000

    created a new redirect html page, expanded flask routes and added more manual test in app.py

--- flaskr/app.py
+++ flaskr/app.py
@@ -3,22 +3,47 @@ from database import DatabaseHandler
                             
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
 def index():
     jobInofList = [ {'job': 'auditor', 'company': 'kpmg', 'salary': 50000}, 
     {'job': 'auditor2', 'company': 'pwc', 'salary': 45000}]
-    #'''
-    if request.method == 'POST':
-        searchFilters = {'searchText': request.form['searchText'],
-                        'salary': { 'min': request.form['salaryMin'],
-                                    'max': request.form['salaryMax']
-                                    },
-                        'jobStartDate': request.form['jobStartDate'],
-                        'applicationStatus': request.form['applicationStatus'],
-                        'jobLocation': request.form['jobLocation']
-                        }
-    #'''
-    print(searchFilters)
     return render_template('index.html', jobInfoList=jobInofList,
                              jobLocations=['glasgow', 'edingburgh'])
+
+
+@app.route('/addJobInfo', methods=['GET', 'POST'])
+def addJobInfo():
+    if request.method == 'POST':
+        jobInfo = {'job': request.form['job'],
+                    'company': request.form['company'],
+                    'salary': request.form['salary'],
+                    'jobLocation': request.form['jobLocation'],
+                    'jobStartDate': request.form['jobStartDate'],
+                    'jobApplicationClosingDate': request.form['jobApplicationClosingDate'],
+                    'applicationStatus': request.form['applicationStatus'],
+                    'notes': request.form['notes']
+                    }
+    return render_template('addJobInfo.html')
+
+
+@app.route('/redirectIndex', methods=['POST'])
+def redirect():
+    requestUrl = str(request.url_rule)
+    if requestUrl == '/redirectIndex':
+        urlToGet = requestUrl
+        searchFilters = getSearchFilters()
+    return render_template('redirect.html', urlToGet=urlToGet)
+
+
+def getSearchFilters():
+    searchFilters = {'searchText': request.form['searchText'],
+                    'salary': { 'min': request.form['salaryMin'],
+                                'max': request.form['salaryMax']
+                                },
+                    'jobStartDate': request.form['jobStartDate'],
+                    'applicationStatus': request.form['applicationStatus'],
+                    'jobLocation': request.form['jobLocation']
+                    }
+    print(searchFilters)
+    return searchFilters
